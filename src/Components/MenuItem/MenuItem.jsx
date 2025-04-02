cf2f787c9270951d75d4459f46ba3ff8094f93d4
chalita <chalita.antonsen@gmail.com>
2025-04-02 13:03:26 +0200

    create base content in components MenuList and MenuItem

--- src/Components/MenuItem/MenuItem.jsx
+++ src/Components/MenuItem/MenuItem.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./MenuItem.module.css";
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+const MenuItem = ({ key, item, cartContent, setCartContent }) => {
+  return (
+    <div className={styles.menuItem}>
+      <img src={item.imageUrl} />
+      <button>Add to Cart</button>
+    </div>
+  );
 };
 
 export default MenuItem;
