5eb90a5b5acda16a5e409a30b136a45ee99fce8e
chalita <chalita.antonsen@gmail.com>
2025-04-02 13:06:02 +0200

    add the rest of the commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import MenuList from "./Components/MenuList/MenuList";
+import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 
 function App() {
-  return <></>;
+  const [cartContent, setCartContent] = useState([]);
+
+  return (
+    <>
+      <section className="listWrapper">
+        <MenuList
+          cartContent={cartContent}
+          setCartContent={setCartContent}
+        ></MenuList>
+      </section>
+
+      <ShoppingCart></ShoppingCart>
+    </>
+  );
 }
 
 export default App;
